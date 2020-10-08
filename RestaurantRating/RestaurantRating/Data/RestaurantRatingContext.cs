bbe222397e19812ce62bb5902fdd6880522a40cd
LiorVais <larrymv@gmail.com>
2020-10-08 17:21:54 +0300

    First Models

---
+++ RestaurantRating/RestaurantRating/Data/RestaurantRatingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RestaurantRating.Models;
+
+namespace RestaurantRating.Data
+{
+    public class RestaurantRatingContext : DbContext
+    {
+        public RestaurantRatingContext (DbContextOptions<RestaurantRatingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RestaurantRating.Models.Restaurant> Restaurant { get; set; }
+    }
+}
---
+++ RestaurantRating/RestaurantRating/Migrations/20201008140819_init.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantRating.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    PriceLevel = table.Column<int>(nullable: false),
+                    City = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurant");
+        }
+    }
+}
