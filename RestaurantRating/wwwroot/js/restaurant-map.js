4e1f4f5dc8e9127cd3a0ec44e15ccfb5298543fe
LiorVais <larrymv@gmail.com>
2020-11-11 01:13:10 +0200

    added map to details page

---
+++ RestaurantRating/wwwroot/js/restaurant-map.js
@@ -0,0 +1,13 @@
+﻿
+const position = new google.maps.LatLng(lat, lon);
+const mapOptions = {
+    mapTypeId: 'roadmap',
+    center: position,
+    zoom: 17
+};
+const map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+map.setTilt(45);
+const marker = new google.maps.Marker({ position, map, title });
+const infoWindow = new google.maps.InfoWindow();
+infoWindow.setContent(title);
+infoWindow.open(map, marker);
